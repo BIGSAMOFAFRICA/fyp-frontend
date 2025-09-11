7ca4e927c3da527c7f8770f3d91ab449b1b31e49
BIGSAMOFAFRICA <olabisisamuelayomide@gmail.com>
2025-09-11 21:44:17 +0100

    api fixed

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://fyp-backend-9t8k.onrender.com/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://fyp-backend-9t8k.onrender.com',
   withCredentials: true,
 });
 
