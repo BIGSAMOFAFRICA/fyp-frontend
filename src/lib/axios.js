b5c08a7cf4253667165f893cbb7b1d81cdecdfdb
BIGSAMOFAFRICA <olabisisamuelayomide@gmail.com>
2025-09-11 22:05:47 +0100

    fixxed

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://fyp-backend-9t8k.onrender.com',
+  baseURL: import.meta.env.VITE_API_URL || 'https://fyp-backend-9t8k.onrender.com/api',
   withCredentials: true,
 });
+
 
 // Add request interceptor to add Authorization header
 instance.interceptors.request.use(
